b54597a394d75c3a02c5a5868666bb8a9f57198e
gulsahdursun <gulsahbasboga@gmail.com>
2021-01-24 17:46:25 +0300

    Method Ödevi

---
+++ ClassMethodDemo/CustomerManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace ClassMethodDemo
+{
+    public class CustomerManager
+    {
+        public void AddCustomer(Customer customer)
+        {
+            Console.WriteLine("------------------------------------------------------------------");
+            Console.WriteLine("Customer successfully added. Information; ");
+            Console.WriteLine("Customer Id: " + customer.Id);
+            Console.WriteLine("Customer Name Surname: " + customer.Name + " " + customer.Surname);
+            Console.WriteLine("Customer Address: " + customer.Address);
+            Console.WriteLine("------------------------------------------------------------------");
+        }
+
+        public void DelCustomer (Customer customer)
+        {
+            Console.WriteLine("Customer successfully deleted. " + customer.Id);
+
+        }
+
+        public void ListCustomer (Customer customer)
+        {
+            Console.WriteLine("******************************************************************");
+            Console.WriteLine("List Of Customer: ");
+            Console.WriteLine("Customer Id: " + customer.Id);
+            Console.WriteLine("Customer Name: " + customer.Name);
+            Console.WriteLine("Customer Surname: " + customer.Surname);
+            Console.WriteLine("Customer Address: " + customer.Address);
+            Console.WriteLine("******************************************************************");
+        }
+    }
+}
---
+++ ClassMethodDemo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ClassMethodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer
+            {
+                Id = 1,
+                Name = "Gülşah",
+                Surname = "Başboğa",
+                Address = "Kadıköy"
+            };
+
+            Customer customer2 = new Customer
+            {
+                Id = 2,
+                Name = "Kübra",
+                Surname = "Başboğa",
+                Address = "Kadıköy"
+            };
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.AddCustomer(customer1);
+            customerManager.AddCustomer(customer2);
+
+            customerManager.ListCustomer(customer1);
+            customerManager.ListCustomer(customer2);
+
+            customerManager.DelCustomer(customer1);
+            customerManager.DelCustomer(customer2);
+
+        }
+    }
+}

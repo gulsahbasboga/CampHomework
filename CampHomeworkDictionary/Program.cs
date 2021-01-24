5a3a9cb6c7fd945483c8f13873669ab820cbe0a6
gulsahdursun <gulsahbasboga@gmail.com>
2021-01-24 22:03:17 +0300

    Dictionary Odevi

---
+++ CampHomeworkDictionary/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CampHomeworkDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Students<int, string> students = new Students<int, string>();
+            students.Add(1001, "Gülşah Başboğa");
+            students.Add(1002, "Mırmır Kedisi");
+            students.Add(1003, "Şeftali Kedisi");
+
+            Console.WriteLine("Student's Number: " + students.Items1[0] + " " + "Student's Name: " + students.Items2[0]);
+            Console.WriteLine("Student's Number: " + students.Items1[1] + " " + "Student's Name: " + students.Items2[1]);
+            Console.WriteLine("Student's Number: " + students.Items1[2] + " " + "Student's Name: " + students.Items2[2]);
+
+        }
+    }
+}
---
+++ CampHomeworkDictionary/Students.cs
@@ -0,0 +1,47 @@
+﻿using System;
+namespace CampHomeworkDictionary
+{
+    public class Students<TKey, TValue>
+    {
+        TKey[] keyArray;
+        TValue[] valueArray;
+        TKey[] tempKeyArray;
+        TValue[] tempValueArray;
+
+        public Students()
+        {
+            keyArray = new TKey[0];
+            valueArray = new TValue[0];
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            tempKeyArray = keyArray;
+            tempValueArray = valueArray;
+
+            keyArray = new TKey[keyArray.Length + 1];
+            valueArray = new TValue[valueArray.Length + 1];
+
+            for (int i = 0; i < tempKeyArray.Length; i++)
+            {
+                keyArray[i] = tempKeyArray[i];
+            }
+
+            for (int i = 0; i < tempValueArray.Length; i++)
+            {
+                valueArray[i] = tempValueArray[i];
+            }
+        }
+
+        public TKey[] Items1
+        {
+            get { return keyArray; }
+        }
+
+        public TValue[] Items2
+        {
+            get { return valueArray; }
+        }
+
+    }
+}
